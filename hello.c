a2cac19af749c8dac450f932524c99b72c9e068f
Louis Antinoro <louantinoro@gmail.com>
2023-08-24 10:13:51 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -4,15 +4,18 @@
  *
  * Date: 2023/08/24
  *
- * A simple hello world program in C
+ * A simple printing program in C
  *
  */
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Louis Antinoro\n");
+  printf("Louis Antinoro!\n");
+  printf("My major is Electrical Engineering\n");
 
   return 0;
 }
