2e76d773659fb029911b73c3d71dbda72122f5d8
Louis Antinoro <louantinoro@gmail.com>
2023-08-24 09:39:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: NAME Louis Antinoro
+ *         EMAIL lantinoro2@unl.edu
+ *
+ * Date: 2023/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Louis Antinoro\n");
+
+  return 0;
+}
